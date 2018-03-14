b67d82c0b584822ab9b19d6160c190772a256704
MonteWa <monte.wang0108@gmail.com>
2018-03-14 15:35:42 +0100

    change

--- main_with_pascal.py
+++ main_with_pascal.py
@@ -7,79 +7,113 @@ import os
 import pandas as pd
 from keras.preprocessing import image
 import scipy.io
-from scipy.misc import imresize
+from skimage.transform import resize
 import gc
 
-# get the path of the data
-data_path = os.path.join('Sitting','img')
-label_path = os.path.join('Sitting','masks')
-data_names = sorted(os.listdir(data_path))
-label_names = sorted(os.listdir(label_path))
-data_addr = []
-for i in range(len(data_names)):
-    data_addr.append(os.path.join('Sitting/img',data_names[i]))
+# read the all names of files that contains person
+# get the path of masks and the path of data
+person_names_mat = os.listdir('VOC2010/masks')
+person_names_jpg = []
+for i in range(len(person_names_mat)):
+    temp = person_names_mat[i][:-4]+'.jpg'
+    person_names_jpg.append(temp)
+mat_path = []
+for i in range(len(person_names_mat)):
+    path = os.path.join('VOC2010/masks',person_names_mat[i])
+    mat_path.append(path)
+jpg_path = []
+for i in range(len(person_names_jpg)):
+    path = os.path.join('VOC2010/JPEGImages',person_names_jpg[i])
+    jpg_path.append(path)
+data_table = pd.DataFrame(jpg_path,columns=['jpg_path'])
+mat_path_df = pd.DataFrame(mat_path,columns=['mat_path'])
+data_table = pd.concat([data_table,mat_path_df],axis=1)
+data_table.head()
 
-label_addr = []
-for i in range(len(label_names)):
-    label_addr.append(os.path.join('Sitting/masks',label_names[i]))
-The_Main_List = pd.DataFrame(data_names,columns = ['data_name'])
-temp_frame1 = pd.DataFrame(label_names,columns=['label_name'])
-temp_frame2 = pd.DataFrame(data_addr,columns=['data_addr'])
-temp_frame3 = pd.DataFrame(label_addr,columns=['label_addr'])
-The_Main_List = pd.concat([The_Main_List,temp_frame1],axis=1)
-The_Main_List = pd.concat([The_Main_List,temp_frame2],axis=1)
-The_Main_List = pd.concat([The_Main_List,temp_frame3],axis=1)
-print(The_Main_List.head(10))
+def squre_crop(img,mask):
+    # overlap img and mask then crop
+    overlap = np.zeros([img.shape[0],img.shape[1],img.shape[2]+1])
+    overlap[:,:,0:3] = img
+    overlap[:,:,3] = mask
+    # random crop
+    height = overlap.shape[0]
+    width = overlap.shape[1]
+    if height%2 != 0:
+        height = height - 1
+    if width%2 != 0:
+        width = width - 1
+    if width>height:
+        range1 = [0,height]
+        range2 = [width-height,width]
+        range3 = [int(width/2-height/2),int(width/2+height/2)]
+        crop1 = overlap[:,range1[0]:range1[1],:]
+        crop2 = overlap[:,range2[0]:range2[1],:]
+        crop3 = overlap[:,range3[0]:range3[1],:]
+    else:
+        range1 = [0,width]
+        range2 = [height-width,height]
+        range3 = [int(height/2-width/2),int(height/2+width/2)]
+        crop1 = overlap[range1[0]:range1[1],:,:]
+        crop2 = overlap[range2[0]:range2[1],:,:]
+        crop3 = overlap[range3[0]:range3[1],:,:]
+    return crop1,crop2,crop3
 
-# load one image
-def load_image(path):
-    img = image.load_img(path,target_size=(256,256))
-    img = image.img_to_array(img)
-    return img
-# laod one label as mask
-def load_mask(path):
-    label = scipy.io.loadmat(path)
-    label = label['M']
-    label_resize = imresize(label,[64,64],interp='nearest')
-    label_resize = label_resize.astype('float32')
-    label_floor = np.floor(label_resize/18)
-    return label_floor
+# read and preprosess the data
+# read img
+Data = np.zeros([3*len(data_table),256,256,4])
+for i in range(len(data_table)):
+    test_img = image.load_img(data_table['jpg_path'][i])
+    test_img = image.img_to_array(test_img)
+    # read mask
+    test_mask = scipy.io.loadmat(data_table['mat_path'][i])['single_map']
+    crop1,crop2,crop3 = squre_crop(test_img,test_mask)
+    re_crop1 = resize(crop1,output_shape=[256,256])
+    re_crop2 = resize(crop2,output_shape=[256,256])
+    re_crop3 = resize(crop3,output_shape=[256,256])
+    Data[i * 3 + 0] = re_crop1
+    Data[i * 3 + 1] = re_crop2
+    Data[i * 3 + 2] = re_crop3
 
-# load all data
-data = np.zeros([len(The_Main_List),256,256,3])
-for i,path in enumerate(The_Main_List.data_addr):
-    data[i] = load_image(path)
-# load all labels
-label = np.zeros([len(The_Main_List),64,64])
-for i,path in enumerate(The_Main_List.label_addr):
-    label[i] = load_mask(path)
-
-# split train and test
-np.random.seed(seed=1993)
-rnd = np.arange(len(data))
-train_idx = rnd < 180
-valid_idx = rnd >= 180
-train_data = data[train_idx]
-test_data = data[valid_idx]
-train_label = label
-test_label = label[valid_idx]
+# split the data into train and validation
+rnd = np.arange(len(Data))
+train_idx = rnd < len(Data)*0.8
+valid_idx = rnd >= len(Data)*0.8
+train_data = Data[train_idx]
+val_data = Data[valid_idx]
 # save some memory
-del data
-del label
+del Data
 gc.collect()
 
-def augmentor_with_keras(img_asarr,mask):
+def get_next_batch(data):
+    length = len(data)
+    idx = np.random.randint(0,length)
+    img = data[idx][:,:,0:3]
+    mask = data[idx][:,:,3]
+    return img,mask
+def augmentor_with_keras(img,mask):
     temp = np.zeros([256,256,4])
-    temp[:,:,0:3] = img_asarr
-    temp[128-32:128+32,128-32:128+32,3]= mask
+    temp[:,:,0:3] = img
+    temp[:,:,3]= mask
     after_rot = image.random_rotation(temp,rg=20,
                                       row_axis=0,col_axis=1,channel_axis=2,fill_mode='constant')
     after_zoom = image.random_zoom(after_rot,zoom_range=[0.7,1.3],
                                    row_axis=0,col_axis=1,channel_axis=2,fill_mode='constant')
     # random_shift
-    return after_zoom[:,:,0:3],after_zoom[128-32:128+32,128-32:128+32,3]
+    return after_zoom[:,:,0:3],after_zoom[:,:,3]
+
+def my_scale(aug_mask):
+    for w in range(256):
+        for h in range(256):
+            if aug_mask[w,h]%1 != 0:
+                aug_mask[w,h] = 0
+    scale_map = np.zeros([64,64])
+    for i in range(64):
+        for j in range(64):
+            scale_map[i,j] = aug_mask[i*4+2,j*4+1]
+    return scale_map
 
 def transfer_mask_to_gtmap(mask):
+    mask = my_scale(mask)
     map = np.zeros([64,64,15])
     for i in range(15):
         for n in range(64):
@@ -88,16 +122,9 @@ def transfer_mask_to_gtmap(mask):
                     map[n,m,i] = 1.0
     # transform to gtMap
     gtmap = np.zeros([8,64,64,15])
-    for i in range(8):
+    for i in range(4):
         gtmap[i] = map
     return gtmap
-
-def get_next_batch(imgs,masks):
-    length = len(imgs)
-    idx = np.random.randint(0,length)
-    img = imgs[idx]
-    mask = masks[idx]
-    return img,mask
 
 def get_augmentor_batch(img,mask,aug_level):
     gtmap = transfer_mask_to_gtmap(mask)
@@ -112,54 +139,37 @@ def get_augmentor_batch(img,mask,aug_level):
         y_batch[i+1] = gtmap
     return x_batch,y_batch
 
-def cal_acc(pred_map,mask):
-    length = mask.shape[0]
-    wideth = mask.shape[1]
-    pixel_acc_list = []
+def cal_acc(pred,mask):
+    length = pred.shape[0]
+    wideth = pred.shape[1]
+    help_arr = np.zeros([length,wideth])
     for i in range(length):
         for j in range(wideth):
-            if mask[i,j]!=0:
-                if pred_map[i,j] == mask[i,j]:
-                    pixel_acc_list.append(1)
-                else:
-                    pixel_acc_list.append(0)
-    acc = np.mean(pixel_acc_list)
+            if pred[i,j] == mask[i,j]:
+                help_arr[i,j] = 1
+    acc = np.mean(help_arr)
     return acc
 
-def cal_iou(pred_map,mask):
-    mask_matrix = np.zeros([64, 64, 15])
-    for i in range(15):
-        for n in range(64):
-            for m in range(64):
-                if mask[n][m] == i:
-                    mask_matrix[n, m, i] = 1.0
-    pred_matrix = np.zeros([64, 64, 15])
-    for i in range(15):
-        for n in range(64):
-            for m in range(64):
-                if (pred_map[n][m] == i).any():
-                    pred_matrix[n, m, i] = 1.0
-    iou_list = []
-    length = mask.shape[0]
-    wideth = mask.shape[1]
+def cal_iou(pred,mask):
+    length = pred.shape[0]
+    wideth = pred.shape[1]
     # find the pixel that at the same time in two map
-    for c in range(15):
-        p_in_two = 0
-        for i in range(length):
-            for j in range(wideth):
-                if (pred_matrix[i,j,c] != 0) and (mask_matrix[i,j,c] != 0):
-                    p_in_two = p_in_two + 1
-        # find the pixel that not equal to zero
-        p_all = 0
-        for i in range(length):
-            for j in range(wideth):
-                if (pred_matrix[i,j,c] != 0) or (mask_matrix[i,j,c] != 0):
-                    p_all = p_all + 1
-        if p_all!= 0:
-            part_iou = p_in_two/p_all
-            iou_list.append(part_iou)
-    result = np.mean(iou_list)
-    return result
+    p_in_two = 0
+    for i in range(length):
+        for j in range(wideth):
+            if (pred[i,j] != 0) and (mask[i,j] != 0):
+                p_in_two = p_in_two + 1
+    # find the pixel that not equal to zero
+    p_all = 0
+    for i in range(length):
+        for j in range(wideth):
+            if (pred[i,j] != 0) or (mask[i,j] != 0):
+                p_all = p_all + 1
+    if p_all != 0:
+        iou = p_in_two/p_all
+    else:
+        iou = 0
+    return iou
 
 def process_config(conf_file):
 	"""
@@ -202,7 +212,7 @@ model = HourglassModel(nFeat=params['nfeats'], nStack=params['nstacks'],
                        joints= params['joint_list'],modif=False)
 model.generate_model()
 
-# output: (None,4,64,64,15)
+# output: (None,8,64,64,15)
 output = model.get_output()
 loss = model.loss
 out = output[0,7]
@@ -220,23 +230,26 @@ train_acc_list = []
 train_iou_list = []
 test_acc_list = []
 test_iou_list = []
-for i in range(5001):
-    x_b,y_b = get_next_batch(train_data,train_label)
-    x,y = get_augmentor_batch(x_b,y_b,6)
+for i in range(1000):
+    x_b,y_b = get_next_batch(train_data)
+    x,y = get_augmentor_batch(x_b,y_b,1)
     sess.run(train_step,feed_dict={model.img:x, model.gtMaps:y})
     if i%50 == 0:
         x_for_eval = np.expand_dims(x_b,axis=0)
         pred_map = sess.run(pred_seg,feed_dict={model.img:x_for_eval})
+        y_b = my_scale(y_b)
         train_acc = cal_acc(pred_map,y_b)
         train_iou = cal_iou(pred_map,y_b)
 
-        test_x,test_y = get_next_batch(test_data,test_label)
+        test_x,test_y = get_next_batch(val_data)
         test_x_for_eva = np.expand_dims(test_x,axis=0)
+        test_y = my_scale(test_y)
         pred_map = sess.run(pred_seg, feed_dict={model.img: test_x_for_eva})
         test_acc = cal_acc(pred_map, test_y)
         test_iou = cal_iou(pred_map, test_y)
 
-        print('iter:',i,'train_acc:',train_acc,'train_iou:',train_iou,'test_acc:',test_acc,'test_iou:',test_iou )
+        print('iter:',i,'train_acc:',train_acc,'train_iou:',train_iou,'val_acc:',test_acc,'val_iou:',test_iou )
+        print('training loss:',sess.run(model.loss, feed_dict={model.img:x, model.gtMaps:y}))
 
         iteration.append(i)
         train_acc_list.append(train_acc)
@@ -245,16 +258,17 @@ for i in range(5001):
         test_iou_list.append(test_iou)
 
 # save iou and acc
+# save iou and acc
 iterdf = pd.DataFrame(iteration,columns=['iter'])
 tradf = pd.DataFrame(train_acc_list,columns=['train_accracy'])
 tridf = pd.DataFrame(train_iou_list,columns=['train_iou'])
 teadf = pd.DataFrame(test_acc_list,columns=['test_accracy'])
 teidf = pd.DataFrame(test_iou_list,columns=['test_iou'])
 eval_info = pd.concat([iterdf,tradf,tridf,teadf,teidf],axis=1)
-eval_info.to_csv('training_info_for_drawing.csv')
+eval_info.to_csv('training_info_with_pascal.csv')
 
 # save the network
-path = "my_net"
+path = "my_net_with_pascal"
 is_exist = os.path.exists(path)
 if not is_exist:
     os.makedirs(path)
--- upsampling_net.py
+++ upsampling_net.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import tensorflow as tf
 import numpy as np
 from hourglass_tiny import HourglassModel
@@ -7,8 +8,6 @@ import pandas as pd
 from keras.preprocessing import image
 import scipy.io
 from scipy.misc import imresize
-import matplotlib.pyplot as plt
-import math
 from tqdm import tqdm
 
 # load data(we need the same inputs but (None,256,256,15) label)
@@ -37,23 +36,14 @@ def load_image(path):
     img = image.load_img(path,target_size=(256,256))
     img = image.img_to_array(img)
     return img
-
-# load one label for upsampling(label size:(None,256,256,15))
-def load_label_as_gtmap(path):
-    # load one label and resize it to [64,64]
+# laod one label as mask
+def load_mask(path):
     label = scipy.io.loadmat(path)
     label = label['M']
     label_resize = imresize(label,[256,256],interp='nearest')
     label_resize = label_resize.astype('float32')
     label_floor = np.floor(label_resize/18)
-    # transform to sigle map
-    map = np.zeros([256,256,15])
-    for i in range(15):
-        for n in range(256):
-            for m in range(256):
-                if label_floor[n][m] == i:
-                    map[n,m,i] = 1.0
-    return map
+    return label_floor
 
 # load all data
 data = np.zeros([len(The_Main_List),256,256,3])
@@ -61,29 +51,108 @@ for i,path in enumerate(The_Main_List.data_addr):
     data[i] = load_image(path)
 
 # load all labels
-label = np.zeros([len(The_Main_List),256,256,15])
+label = np.zeros([len(The_Main_List),256,256])
 for i,path in tqdm(enumerate(The_Main_List.label_addr)):
-    label[i] = load_label_as_gtmap(path)
-
+    label[i] = load_mask(path)
 
 # split train and test,first 80% train,last 20% test.
 rnd = np.arange(len(data))
-train_idx = rnd < (0.8*len(data))
-valid_idx = rnd >= (0.8*len(data))
+train_idx = rnd < 180
+valid_idx = rnd >= 180
 train_data = data[train_idx]
 test_data = data[valid_idx]
 train_label = label[train_idx]
 test_label = label[valid_idx]
 
-def next_batch(data,label,batch_size):
-    batch_x = np.zeros([batch_size,256,256,3])
-    batch_y = np.zeros([batch_size,256,256,15])
-    idx = np.arange(len(data))
-    np.random.shuffle(idx)
-    for i in range(batch_size):
-        batch_x[i] = data[idx[i]]
-        batch_y[i] = label[idx[i]]
-    return batch_x,batch_y
+def augmentor_with_keras(img_asarr,mask):
+    temp = np.zeros([256,256,4])
+    temp[:,:,0:3] = img_asarr
+    temp[:,:,3]= mask
+    after_rot = image.random_rotation(temp,rg=20,
+                                      row_axis=0,col_axis=1,channel_axis=2,fill_mode='constant')
+    after_zoom = image.random_zoom(after_rot,zoom_range=[0.7,1.3],
+                                   row_axis=0,col_axis=1,channel_axis=2,fill_mode='constant')
+    #after_jit = image.random_shift(after_zoom,wrg=0.2,hrg=0.2,
+    #                               row_axis=0,col_axis=1,channel_axis=2,fill_mode='constant')
+    return after_zoom[:,:,0:3],after_zoom[:,:,3]
+
+def transfer_mask_to_map(mask):
+    map = np.zeros([256,256,15])
+    for i in range(15):
+        for n in range(64):
+            for m in range(64):
+                if mask[n][m] == i:
+                    map[n,m,i] = 1.0
+    return map
+
+def next_batch(data,label):
+    length = len(data)
+    idx = np.random.randint(0,length)
+    img = data[idx]
+    mask = label[idx]
+    return img,mask
+
+def get_augmentor_batch(img,mask,aug_level):
+    y_map = transfer_mask_to_map(mask)
+    x_batch = np.zeros([aug_level,256,256,3])
+    y_batch = np.zeros([aug_level,256,256,15])
+    x_batch[0]=img
+    y_batch[0]=y_map
+    for i in range(aug_level-1):
+        aug_img,aug_mask = augmentor_with_keras(img,mask)
+        y_map = transfer_mask_to_map(aug_mask)
+        x_batch[i+1] = aug_img
+        y_batch[i+1] = y_map
+    return x_batch,y_batch
+
+def cal_acc(pred_map,mask):
+    length = mask.shape[0]
+    wideth = mask.shape[1]
+    pixel_acc_list = []
+    for i in range(length):
+        for j in range(wideth):
+            if mask[i,j]!=0:
+                if pred_map[i,j] == mask[i,j]:
+                    pixel_acc_list.append(1)
+                else:
+                    pixel_acc_list.append(0)
+    acc = np.mean(pixel_acc_list)
+    return acc
+
+def cal_iou(pred_map,mask):
+    mask_matrix = np.zeros([256, 256, 15])
+    for i in range(15):
+        for n in range(256):
+            for m in range(256):
+                if mask[n][m] == i:
+                    mask_matrix[n, m, i] = 1.0
+    pred_matrix = np.zeros([256, 256, 15])
+    for i in range(15):
+        for n in range(256):
+            for m in range(256):
+                if (pred_map[n][m] == i).any():
+                    pred_matrix[n, m, i] = 1.0
+    iou_list = []
+    length = mask.shape[0]
+    wideth = mask.shape[1]
+    # find the pixel that at the same time in two map
+    for c in range(15):
+        p_in_two = 0
+        for i in range(length):
+            for j in range(wideth):
+                if (pred_matrix[i,j,c] != 0) and (mask_matrix[i,j,c] != 0):
+                    p_in_two = p_in_two + 1
+        # find the pixel that not equal to zero
+        p_all = 0
+        for i in range(length):
+            for j in range(wideth):
+                if (pred_matrix[i,j,c] != 0) or (mask_matrix[i,j,c] != 0):
+                    p_all = p_all + 1
+        if p_all!= 0:
+            part_iou = p_in_two/p_all
+            iou_list.append(part_iou)
+    result = np.mean(iou_list)
+    return result
 
 # build the model
 def process_config(conf_file):
@@ -151,7 +220,7 @@ variables_to_restore = tf.contrib.slim.get_variables_to_restore()
 # last map: (None,64,64,15)
 output_stack = model.output
 stacks = tf.unstack(output_stack,axis=1)
-last_map = stacks[3]
+last_map = stacks[7]
 # add layers for upsampling, from (64,64,15) to (256,256,15)
 bi_upsampling = tf.image.resize_bilinear(last_map,[256,256],name='bilinear_upsampling')
 W_last = weight_variable([3,3,15,15],name='W_last')
@@ -159,29 +228,62 @@ b_last = bias_variable([15],name='b_last')
 aft_upsam_out = conv2d_basic(bi_upsampling,W_last,b_last)
 loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=aft_upsam_out, labels= model.hr_label),
                       name= 'cross_entropy_loss')
+pred = tf.reduce_mean(aft_upsam_out,axis=0)
+pred = tf.argmax(pred,axis=2)
 
 train_step = tf.train.RMSPropOptimizer(1e-3).minimize(loss)
 sess = tf.Session()
 init = tf.global_variables_initializer()
 sess.run(init)
-# restore the hourglass net
 saver = tf.train.Saver(variables_to_restore)
 path = "my_net"
 full_path = os.path.join(path,'save_net.ckpt')
 saver.restore(sess,full_path)
 
 # training
-for i in range(2000):
-    x, y = next_batch(train_data, train_label, 3)
-    if i/50 == 0:
-        print(sess.run(loss,feed_dict={model.img:x, model.hr_label:y}))
-    sess.run(train_step, feed_dict={model.img: x, model.hr_label: y})
+iteration = []
+train_acc_list = []
+train_iou_list = []
+test_acc_list = []
+test_iou_list = []
+for i in range(1001):
+    x, y = next_batch(train_data, train_label)
+    aug_x, aug_y = get_augmentor_batch(x, y, aug_level=2)
+    sess.run(train_step, feed_dict={model.img: aug_x, model.hr_label: aug_y})
+
+    if i%50 == 0:
+        x_for_eval = np.expand_dims(x, axis=0)
+        train_pred = sess.run(pred, feed_dict={model.img: x_for_eval})
+        train_acc = cal_acc(train_pred, y)
+        train_iou = cal_iou(train_pred, y)
+
+        test_x,test_y = next_batch(test_data,test_label)
+        test_x_for_eva = np.expand_dims(test_x, axis=0)
+        test_pred = sess.run(pred, feed_dict={model.img: test_x_for_eva})
+        test_acc = cal_acc(test_pred, test_y)
+        test_iou = cal_iou(test_pred, test_y)
+        print('iter:', i, 'train_acc:', train_acc, 'train_iou:', train_iou, 'test_acc:', test_acc, 'test_iou:', test_iou)
+        print('loss:',sess.run(loss, feed_dict={model.img: aug_x, model.hr_label: aug_y}))
+        iteration.append(i)
+        train_acc_list.append(train_acc)
+        train_iou_list.append(train_iou)
+        test_acc_list.append(test_acc)
+        test_iou_list.append(test_iou)
+
+# save iou and acc
+iterdf = pd.DataFrame(iteration,columns=['iter'])
+tradf = pd.DataFrame(train_acc_list,columns=['train_accracy'])
+tridf = pd.DataFrame(train_iou_list,columns=['train_iou'])
+teadf = pd.DataFrame(test_acc_list,columns=['test_accracy'])
+teidf = pd.DataFrame(test_iou_list,columns=['test_iou'])
+eval_info = pd.concat([iterdf,tradf,tridf,teadf,teidf],axis=1)
+eval_info.to_csv('training_info_for_drawing.csv')
 
 # save the network
+saver2 = tf.train.Saver()
 path = "my_upsampling_net"
 is_exist = os.path.exists(path)
 if not is_exist:
     os.makedirs(path)
 full_path = os.path.join(path,'save_net.ckpt')
-saver2 = tf.train.Saver()
 save_path = saver2.save(sess,full_path)
